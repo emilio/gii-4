7c98624f88652dfe144caec32ad42af8cafa60d0
Emilio Cobos Álvarez <emilio@crisal.io>
2017-03-31 15:01:06 +0200

    tests: Add some basic evaluator tests.

--- compilers/tests/EvaluatorTest.cc
+++ compilers/tests/EvaluatorTest.cc
@@ -1,4 +1,20 @@
 #include "gtest/gtest.h"
+#include "TestUtils.h"
+
+void assertExprValue(const char* expr, double val) {
+  parse(expr, [val](ast::Node* node, const ParseError* error) {
+    EXPECT_TRUE(toExpression(node)->evaluate().normalizedValue() == val);
+  });
+}
+
+TEST(Evaluator, Basic) {
+  assertExprValue("1 + 1 + 5", 7.0);
+}
+
+TEST(Evaluator, OperatorPrecedence) {
+  assertExprValue("1 + 6 * 5", 31.0);
+  assertExprValue("6 * 2 + 6 * 5", 42.0);
+}
 
 int main(int argc, char** argv) {
   ::testing::InitGoogleTest(&argc, argv);
