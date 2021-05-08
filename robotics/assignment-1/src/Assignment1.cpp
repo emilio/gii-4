15ad499acec622c240ffa0f13838699c89a5a51c
Emilio Cobos Álvarez <emilio@crisal.io>
2021-05-08 02:24:29 +0200

    robotics: Add some code I wrote a while ago and hadn't committed.

--- robotics/assignment-1/src/Assignment1.cpp
+++ robotics/assignment-1/src/Assignment1.cpp
@@ -1,10 +1,61 @@
 #include <opencv2/opencv.hpp>
 #include <iostream>
 #include <optional>
+#include <cassert>
 
-using Rect = cv::Rect2f;
+using Rect = cv::Rect2i;
+using Point = cv::Point2i;
+using Size = cv::Size2i;
+
+template <typename Callback>
+static void iterateAllPoints(const cv::Mat& image, Callback&& cb) {
+  assert(image.channels() == 1);
+  for (int x = 0; x < image.rows; x++) {
+    for (int y = 0; y < image.cols; ++y) {
+      cb(Point(x, y), x * y);
+    }
+  }
+}
+
 static Rect estimatePositionViaBinarization(const cv::Mat& image) {
-  return Rect();
+  assert(image.channels() == 3);
+  // const double kThreshold = 253.0;
+  const int kMax = std::numeric_limits<int>::max();
+  std::vector<cv::Mat> channels;
+  cv::split(image, channels);
+
+  int t = kMax, r = 0, b = 0, l = kMax;
+  const unsigned kBlueThreshold = 50;
+  const unsigned kGreenThreshold = 50;
+  const unsigned kRedThreshold = 250;
+
+  const auto& blueChannel = channels[0];
+  const auto& greenChannel = channels[1];
+  const auto& redChannel = channels[2];
+  assert(channels.size() == 3);
+
+  cv::imshow("BLUE", blueChannel);
+  cv::imshow("GREEN", greenChannel);
+  cv::imshow("RED", redChannel);
+  cv::waitKey();
+
+  // This is some sort of "manual" binary thresholding, to be able to account
+  // for all color channels and not.
+  iterateAllPoints(redChannel, [&](const Point& point, int index) {
+    const unsigned char red = redChannel.data[index];
+    const unsigned char green = greenChannel.data[index];
+    const unsigned char blue = blueChannel.data[index];
+
+    if (red < kRedThreshold || green > kGreenThreshold || blue > kBlueThreshold) {
+      return;
+    }
+
+    t = std::min(point.y, t);
+    r = std::max(point.x, r);
+    b = std::max(point.y, b);
+    l = std::min(point.x, l);
+  });
+  return Rect(Point(t, l), Size(r - l, b - t));
 }
 
 static void compareRects(const Rect& reference, const Rect& estimate) {
